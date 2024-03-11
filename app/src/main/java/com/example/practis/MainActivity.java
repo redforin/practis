94aa2fadf44c99678c09e1a5820d0fbd46044bfb
user05 <10.207.145.22:3000>
2024-03-11 17:34:55 +0300

    practice day 1

---
+++ app/src/main/java/com/example/practis/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.practis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.window.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}

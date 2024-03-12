852ded997e7945be115381a65fd90ad6c032964b
user05 <10.207.145.22:3000>
2024-03-12 17:11:33 +0300

    practice day 2

--- app/src/main/java/com/example/practis/SecondActivity.java
+++ app/src/main/java/com/example/practis/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.practis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void onClick(View view){
+        Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClicked(View view){
+        Intent intent = new Intent(SecondActivity.this, FourActivity.class);
+        startActivity(intent);
     }
 }

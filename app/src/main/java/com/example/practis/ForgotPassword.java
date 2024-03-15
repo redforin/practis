d8673f10733411900fb4fd15ee34b02ee87dee51
redforin <galakxova24@gmail.com>
2024-03-15 19:13:16 +0300

    prcactice day 5

--- app/src/main/java/com/example/practis/ForgotPassword.java
+++ app/src/main/java/com/example/practis/ForgotPassword.java
@@ -18,4 +18,9 @@ public class ForgotPassword extends AppCompatActivity {
         Intent intent = new Intent(ForgotPassword.this, SingUp.class);
         startActivity(intent);
     }
+
+    public void onNexts(View view){
+        Intent intent = new Intent(ForgotPassword.this, OTP.class);
+        startActivity(intent);
+    }
 }

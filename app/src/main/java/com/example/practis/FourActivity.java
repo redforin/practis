16c11e56b7e17f937365222d9706a299dd142bd1
redforin <galakxova24@gmail.com>
2024-03-15 15:37:03 +0300

    prcactice day 5

--- app/src/main/java/com/example/practis/FourActivity.java
+++ app/src/main/java/com/example/practis/FourActivity.java
@@ -2,7 +2,9 @@ package com.example.practis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FourActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FourActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four);
+    }
+
+    public void onNexts(View view){
+        Intent intent = new Intent(FourActivity.this, SingUp.class);
+        startActivity(intent);
     }
 }
